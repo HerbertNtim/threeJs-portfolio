98ab22312961bce90e73dd18252e99d8ae9a9fee
Herbert <herbetntim@gmail.com>
2024-11-12 12:11:56 +0000

    finished with the hero title

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => (
+  <section className="w-full min-h-screen flex flex-col relative">
+    <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+      <p className="sm:text-3xl text-2xl font-medium dark:text-white text-center font-generalsans">Hi, I am Herbert <span className="waving-hand">👋</span></p>
+      <p className="hero_tag text-gray_gradient-light dark:text-gray_gradient">Transforming Ideas into Reality</p>
+    </div>
+  </section>
+)
+
+export default Hero
