4a0e77de56baede7221daf6ba083ae9dd5790dfa
Herbert <herbetntim@gmail.com>
2024-11-14 15:58:59 +0000

    emailJS implemented

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,7 +1,8 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 
-import Alert from '../components/Alert.jsx';
-import useAlert from '../hook/useAlert.js';
+import Alert from "../components/Alert.jsx";
+import useAlert from "../hook/useAlert.js";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const formRef = useRef();
@@ -9,44 +10,43 @@ const Contact = () => {
   const { alert, showAlert, hideAlert } = useAlert();
   const [loading, setLoading] = useState(false);
 
-  const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
 
   const handleChange = ({ target: { name, value } }) => {
     setForm({ ...form, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
-      .send(
+    await emailjs.send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
-          from_name: form.name,
-          to_name: 'JavaScript Mastery',
+          user_name: form.name,
+          to_name: "Herbert",
+          user_message: form.message,
           from_email: form.email,
-          to_email: 'sujata@jsmastery.pro',
-          message: form.message,
+          to_email: "herbertntim2023@gmail.com" 
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
           setLoading(false);
           showAlert({
             show: true,
-            text: 'Thank you for your message 😃',
-            type: 'success',
+            text: "Thank you for your message 😃",
+            type: "success",
           });
 
           setTimeout(() => {
             hideAlert(false);
             setForm({
-              name: '',
-              email: '',
-              message: '',
+              name: "",
+              email: "",
+              message: "",
             });
           }, [3000]);
         },
@@ -57,9 +57,9 @@ const Contact = () => {
           showAlert({
             show: true,
             text: "I didn't receive your message 😢",
-            type: 'danger',
+            type: "danger",
           });
-        },
+        }
       );
   };
 
@@ -68,15 +68,25 @@ const Contact = () => {
       {alert.show && <Alert {...alert} />}
 
       <div className="relative min-h-screen flex items-center justify-center flex-col">
-        <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
+        <img
+          src="/assets/terminal.png"
+          alt="terminal-bg"
+          className="absolute inset-0 min-h-screen"
+        />
 
         <div className="contact-container">
           <h3 className="head-text">Let's talk</h3>
           <p className="text-lg text-white-600 mt-3">
-            Whether you&apos;re looking to build a new software application, improve your existing platform, or bring a unique project to life, I&apos;m here to help.
+            Whether you&apos;re looking to build a new software application,
+            improve your existing platform, or bring a unique project to life,
+            I&apos;m here to help.
           </p>
 
-          <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="mt-12 flex flex-col space-y-7"
+          >
             <label className="space-y-3">
               <span className="field-label">Full Name</span>
               <input
@@ -117,9 +127,13 @@ const Contact = () => {
             </label>
 
             <button className="field-btn" type="submit" disabled={loading}>
-              {loading ? 'Sending...' : 'Send Message'}
+              {loading ? "Sending..." : "Send Message"}
 
-              <img src="/assets/arrow-up.png" alt="arrow-up" className="field-btn_arrow" />
+              <img
+                src="/assets/arrow-up.png"
+                alt="arrow-up"
+                className="field-btn_arrow"
+              />
             </button>
           </form>
         </div>
