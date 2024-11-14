13af73b1c442d39c8848626759c056d5b5f8a69b
Herbert <herbetntim@gmail.com>
2024-11-14 11:10:29 +0000

    updated the client

--- src/sections/Clients.jsx
+++ src/sections/Clients.jsx
@@ -16,15 +16,18 @@ const Clients = () => {
                   <img src={item.img} alt="reviewer" className="w-12 h-12 rounded-full" />
                   <div className="flex flex-col">
                     <p className="font-semibold text-white-800">{item.name}</p>
-                    <p className="text-white-500 md:text-base text-sm font-light">{item.position}</p>
+                    <p className="text-white-600 md:text-base text-sm font-light">{item.position}</p>
                   </div>
                 </div>
 
-                <div className="flex self-end items-center gap-2">
+                {/* <div className="flex self-end items-center gap-2">
                   {Array.from({ length: 5 }).map((_, index) => (
                     <img key={index} src="/assets/star.png" alt="star" className="w-5 h-5" />
                   ))}
-                </div>
+                </div> */}
+                <a href="" className="dark:text-white-700">
+                  LinkedIn
+                </a>
               </div>
             </div>
           </div>
