2a90c2c0ae12519eb58c4becb2537894181b6f7b
Herbert <herbetntim@gmail.com>
2024-11-15 12:02:56 +0000

    added experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./sections/About"
 import Clients from "./sections/Clients"
 import Contact from "./sections/Contact"
+import WorkExperience from "./sections/Experience"
 import Footer from "./sections/Footer"
 import Hero from "./sections/Hero"
 import Navbar from "./sections/Navbar"
@@ -13,6 +14,7 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
+      <WorkExperience />
       <Clients />
       <Contact />
       <Footer />
